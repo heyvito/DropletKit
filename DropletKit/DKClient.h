25d0209bd189ab0ff530260cc1d268357fdfe4c0
Victor Gama <hey@vito.io>
2015-11-21 17:09:01 -0200

    Add documentation for DKClient

--- DropletKit/DKClient.h
+++ DropletKit/DKClient.h
@@ -11,13 +11,45 @@
 @class AFHTTPSessionManager, PMKPromise;
 
 @interface DKClient : NSObject
+
+/**
+ *  Gets or sets the authenticationToken for the requests made with the library
+ */
 @property NSString *authenticationToken;
-@property NSNumber *requestsPerHour;
-@property NSNumber *requestsLeft;
-@property NSDate *rateLimitReset;
+
+/**
+ *  Automatically updated, indicates how many requests per hour DigitalOcean
+ *  offers.
+ */
+@property (readonly) NSNumber *requestsPerHour;
+
+/**
+ *  Automatically updated, indicates how many requests your authenticationToken
+ *  can still issue.
+ */
+@property (readonly) NSNumber *requestsLeft;
+
+/**
+ *  Automatically updated, indicates when the rate limit for the current
+ *  authenticationToken will be reset.
+ */
+@property (readonly) NSDate *rateLimitReset;
 @property NSDictionary *requestHeaders;
 
+/**
+ *  Sets the access token used by the application to perform requests on
+ *  behalf a user.
+ *
+ *  @param token Token value acquired through the OAuth authentication method,
+ *               generated on the Control Panel
+ */
 + (void)setAuthenticationToken:(NSString *)token;
+
+/**
+ *  Returns the DropletKit singleton instance
+ *
+ *  @return The shared DKClient instance
+ */
 + (instancetype)sharedInstance;
 
 - (NSString *)apiURLForEndpoint:(NSString *)endpoint;
@@ -32,20 +64,125 @@
 - (PMKPromise *)requestAction:(NSString *)action forUrl:(NSString *)url;
 - (PMKPromise *)requestAction:(NSString *)action forUrl:(NSString *)url andData:(NSDictionary *)data;
 
-
+/**
+ *  Gets information about the authenticated account
+ *
+ *  @return A `PMKPromise` that will be fulfilled whenever the requests
+ *          succeeds or fails.
+ *  @warning Parameter type for `then` is `DKUser *`.
+ */
 - (PMKPromise *)getAccountInfo;
+
+/**
+ *  Gets a list of droplets owned by the authenticated
+ *  user
+ *
+ *  @return A `PMKPromise` that will be fulfilled whenever the requests
+ *          succeeds or fails.
+ *  @warning Parameter type for `then` is `DKDropletCollection *`
+ */
 - (PMKPromise *)getDroplets;
+
+/**
+ *  Gets a list of actions performed on droplets, including their
+ *  start time and status.
+ *
+ *  @return A `PMKPromise` that will be fulfilled whenever the requests
+ *          succeeds or fails.
+ *  @warning Parameter type for `then` is `DKActionCollection *`
+ */
 - (PMKPromise *)getActions;
+
+/**
+ *  Gets an action with a given ID
+ *
+ *  @param actionId ID of the action to be get
+ *
+ *  @return A `PMKPromise` that will be fulfilled whenever the requests
+ *          succeeds or fails.
+ *  @warning Parameter type for `then` is `DKAction *`
+ */
 - (PMKPromise *)getActionWithId:(NSNumber *)actionId;
+
+/**
+ *  Gets a list of domains held by the authenticated user.
+ *
+ *  @return A `PMKPromise` that will be fulfilled whenever the requests
+ *          succeeds or fails.
+ *  @warning Parameter type for `then` is `DKDomainCollection *`
+ */
 - (PMKPromise *)getDomains;
 
 
+/**
+ *  Gets a list of available images for the authenticated user. This list
+ *  may contain up to three different kinds of images:
+ *  1. An automatic backup of a Droplet
+ *  2. An snapshot taken from a Droplet
+ *  3. Public Linux distribution or Application Image used as base to create
+ *  new droplets
+ *
+ *  @return A `PMKPromise` that will be fulfilled whenever the requests
+ *          succeeds or fails.
+ *  @warning Parameter type for `then` is `DKImageCollection *`
+ */
 - (PMKPromise *)getImages;
+
+/**
+ *  Gets a list of available distribution images. Those images are base Linux
+ *  installations used to create new Droplets.
+ *
+ *  @return A `PMKPromise` that will be fulfilled whenever the requests
+ *          succeeds or fails.
+ *  @warning Parameter type for `then` is `DKImageCollection *`
+ */
 - (PMKPromise *)getDistributionImages;
+
+/**
+ *  Gets a list of available application images. Those images are base application
+ *  installations used to create new Droplets.
+ *
+ *  @return A `PMKPromise` that will be fulfilled whenever the requests
+ *          succeeds or fails.
+ *  @warning Parameter type for `then` is `DKImageCollection *`
+ */
 - (PMKPromise *)getApplicationImages;
+
+/**
+ *  Gets a list of images held by the authenticated user. Those images represent
+ *  snapshots and backups of droplets.
+ *
+ *  @return A `PMKPromise` that will be fulfilled whenever the requests
+ *          succeeds or fails.
+ *  @warning Parameter type for `then` is `DKImageCollection *`
+ */
 - (PMKPromise *)getUserImages;
 
+/**
+ *  Gets all Public SSH keys for the authenticated user.
+ *
+ *  @return A `PMKPromise` that will be fulfilled whenever the requests
+ *          succeeds or fails.
+ *  @warning Parameter type for `then` is `DKSSHKeyCollection *`
+ */
 - (PMKPromise *)getSSHKeys;
+
+/**
+ *  Gets a list of DigitalOcean's DataCenters where Droplets can be
+ *  deployed and images can be tranferred.
+ *
+ *  @return A `PMKPromise` that will be fulfilled whenever the requests
+ *          succeeds or fails.
+ *  @warning Parameter type for `then` is `DKRegionCollection *`
+ */
 - (PMKPromise *)getRegions;
+
+/**
+ *  Gets a list of packages of hardware resources that can be used by Droplets
+ *
+ *  @return A `PMKPromise` that will be fulfilled whenever the requests
+ *          succeeds or fails.
+ *  @warning Parameter type for `then` is `DKSizeCollection *`
+ */
 - (PMKPromise *)getSizes;
 @end
