0b0b4b12471a2cf94942904a7e93c193e1a361ab
Victor Gama <hey@vito.io>
2015-11-21 04:11:59 -0200

    Initial Commit

---
+++ DropletKit/DropletKit.h
@@ -0,0 +1,19 @@
+//
+//  DropletKit.h
+//  DropletKit
+//
+//  Created by Victor Gama on 11/21/15.
+//  Copyright © 2015 Hexic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DropletKit.
+FOUNDATION_EXPORT double DropletKitVersionNumber;
+
+//! Project version string for DropletKit.
+FOUNDATION_EXPORT const unsigned char DropletKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DropletKit/PublicHeader.h>
+
+
