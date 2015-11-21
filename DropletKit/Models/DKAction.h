96eaad2731c968cf591b8619f63d9ff4478ff3f8
Victor Gama <hey@vito.io>
2015-11-21 17:15:06 -0200

    Add documentation to DKAction

--- DropletKit/Models/DKAction.h
+++ DropletKit/Models/DKAction.h
@@ -11,17 +11,52 @@
 @interface DKAction : DKBaseModel
 
 #pragma mark Common properties
+
+/**
+ *  The current status of the action.
+ *  Possible values are "in-progress", "completed", and "errored".
+ */
 @property NSString *status;
+
+/**
+ *  This is the type of action that the object represents.
+ *  For example, this could be "transfer" to represent the state of an
+ *  image transfer action.
+ */
 @property NSString *type;
 
 #pragma mark Renamed properties
+
+/**
+ *  A unique numeric ID that can be used to identify and reference an action.
+ */
 @property NSNumber *actionId;
+
+/**
+ *  A unique identifier for the resource that the action is associated with.
+ */
 @property NSNumber *resourceId;
+
+/**
+ *  The type of resource that the action is associated with.
+ */
 @property NSString *resourceType;
+
+/**
+ *  A slug representing the region where the action occurred.
+ */
 @property NSString *regionSlug;
 
 #pragma mark Extended properties
+
+/**
+ *  A date and time that represents when the action was initiated.
+ */
 @property NSDate *startedAt;
+
+/**
+ *  A date and time that represents when the action was completed.
+ */
 @property NSDate *completedAt;
 
 @end
